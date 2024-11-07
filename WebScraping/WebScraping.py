4691da201bf751cf33085619c20ea53affc3d820
Xin Wei <120625399+X1nW@users.noreply.github.com>
2024-11-07 02:45:56 -0500

    Add files via upload

--- WebScraping/WebScraping.py
+++ WebScraping/WebScraping.py
@@ -1,70 +1,109 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
 
-# Initalize WebDriver
+df = pd.read_csv("list1.csv")
+authors = df["0"].tolist() 
+authors = ["SUSAN WALKER", "NEIL MYLER"]
+# Initialzie WebDriver
 service = Service('./chromedriver')
 driver = webdriver.Chrome(service=service)
 
 # Open OpenBU
-driver.get('https://open.bu.edu/discover')  
-
+driver.get('https://open.bu.edu/discover')
 time.sleep(2)
 
-# Step 1: Press "Show Advanced Filters" link
+# Ensure the page is loaded
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "show-advanced-filters"))
+)
+
+# Show Advanced Filters
 show_advanced_filters = driver.find_element(By.CLASS_NAME, "show-advanced-filters")
-show_advanced_filters.click()
-time.sleep(1)  
+driver.execute_script("arguments[0].scrollIntoView(true);", show_advanced_filters)
+time.sleep(1)
+driver.execute_script("arguments[0].click();", show_advanced_filters)
+time.sleep(1)
 
-# Author name list
-authors = ["MAYANK VARIA"]  
+# Change "Title" to "Author"
+filter_dropdown = driver.find_element(By.ID, "aspect_discovery_SimpleSearch_field_filtertype_1")
+for option in filter_dropdown.find_elements(By.TAG_NAME, 'option'):
+    if option.get_attribute("value") == "author":
+        option.click()
+        break
 
+
+# Empty list
 results = []
 
 for author in authors:
-    # Step 2: select "Author" as filter
-    filter_dropdown = driver.find_element(By.ID, "aspect_discovery_SimpleSearch_field_filtertype_1")
-    for option in filter_dropdown.find_elements(By.TAG_NAME, 'option'):
-        if option.get_attribute("value") == "author":
-            option.click()
-            break
+    input_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "aspect_discovery_SimpleSearch_field_filter_1"))
+    )
+    driver.execute_script("arguments[0].value = '';", input_field) #clear
+    input_field.send_keys(author)  #input
 
-    # Step 3: Fill in authors' name
-    input_field = driver.find_element(By.ID, "aspect_discovery_SimpleSearch_field_filter_1")
-    input_field.clear()  
-    input_field.send_keys(author)
-    
-    # Step 4: Apply to search
+    # Apply
     apply_button = driver.find_element(By.ID, "aspect_discovery_SimpleSearch_field_submit_apply_filter")
     apply_button.click()
-    
-    time.sleep(2) 
+    time.sleep(2)
 
-    # Step 5: Scrape result of current page
+    # Scraping
     while True:
-        items = driver.find_elements(By.CLASS_NAME, "ds-artifact-title")  
+        items = driver.find_elements(By.TAG_NAME, "h4")  # target title
         for item in items:
-            title = item.text
-            results.append({"Author": author, "Title": title})
+            try:
+                item.click()
+                time.sleep(2)
 
-        # Step 6: Check is there is "Next"
+                # title
+                title_element = driver.find_element(By.CLASS_NAME, "page-header")
+                title = title_element.text if title_element else None
+
+                # abstract
+                try:
+                    abstract_element = driver.find_element(By.XPATH, "//h5[text()='Abstract']/following-sibling::div")
+                    abstract = abstract_element.text if abstract_element else None
+                except:
+                    abstract = None  # if no abstract, set as none
+
+                # Ignore poster
+                if title or abstract:
+                    results.append({"Author": author, "Title": title, "Abstract": abstract})
+                
+                # back to search page
+                driver.back()
+                time.sleep(2)
+
+            except:
+                continue 
+
+        # Check if there is a Next
         try:
-            next_button = driver.find_element(By.LINK_TEXT, "Next")  
+            next_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "glyphicon-arrow-right"))
+            )
             next_button.click()
-            time.sleep(2)  
+            time.sleep(2)
         except:
-            break  
+            break  # If no, break the loop
 
-    # Step 7: Reset
-    reset_button = driver.find_element(By.XPATH, "//button[text()='Reset']")
-    reset_button.click()
+    # After scrapping one author's all papers, jumpy to the next
+    show_advanced_filters = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "show-advanced-filters"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView(true);", show_advanced_filters)
+    driver.execute_script("arguments[0].click();", show_advanced_filters)
     time.sleep(1)
 
+# Quit chrome
 driver.quit()
 
 # Save as csv
 df = pd.DataFrame(results)
 df.to_csv("./author_search_results.csv", index=False)
-print("Data in 'author_search_results.csv'")
+print(" Result in author_search_results.csv'")
